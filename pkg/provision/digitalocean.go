7d85d2fd46bb5743c349a0ddbd68094925fb8ffb
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2019-10-05 10:46:25 +0100

    Break-out provisioning package
    Provisioning code abstracted and de-duplicated into new packages
    for easier extension and implementation of new provisioners.
    
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

---
+++ pkg/provision/digitalocean.go
@@ -0,0 +1,90 @@
+package provision
+
+import (
+	"context"
+	"fmt"
+	"strconv"
+
+	"github.com/digitalocean/godo"
+	"golang.org/x/oauth2"
+)
+
+type DigitalOceanProvisioner struct {
+	client *godo.Client
+}
+
+func NewDigitalOceanProvisioner(accessKey string) (*DigitalOceanProvisioner, error) {
+
+	tokenSource := &TokenSource{
+		AccessToken: accessKey,
+	}
+	oauthClient := oauth2.NewClient(context.Background(), tokenSource)
+	client := godo.NewClient(oauthClient)
+
+	return &DigitalOceanProvisioner{
+		client: client,
+	}, nil
+}
+
+func (p *DigitalOceanProvisioner) Status(id string) (*ProvisionedHost, error) {
+	sid, _ := strconv.Atoi(id)
+
+	droplet, _, err := p.client.Droplets.Get(context.Background(), sid)
+
+	if err != nil {
+		return nil, err
+	}
+
+	state := droplet.Status
+
+	ip := ""
+	for _, network := range droplet.Networks.V4 {
+		if network.Type == "public" {
+			ip = network.IPAddress
+		}
+	}
+
+	return &ProvisionedHost{
+		ID:     fmt.Sprintf("%s", droplet.ID),
+		Status: state,
+		IP:     ip,
+	}, nil
+}
+
+func (p *DigitalOceanProvisioner) Provision(host BasicHost) (*ProvisionedHost, error) {
+
+	if host.Region == "" {
+		host.Region = "lon1"
+	}
+
+	createReq := &godo.DropletCreateRequest{
+		Name:   host.Name,
+		Region: host.Region,
+		Size:   host.Plan,
+		Image: godo.DropletCreateImage{
+			Slug: host.OS,
+		},
+		UserData: host.UserData,
+	}
+	droplet, _, err := p.client.Droplets.Create(context.Background(), createReq)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &ProvisionedHost{
+		ID: fmt.Sprintf("%s", droplet.ID),
+	}, nil
+
+}
+
+type TokenSource struct {
+	AccessToken string
+}
+
+func (t *TokenSource) Token() (*oauth2.Token, error) {
+	token := &oauth2.Token{
+		AccessToken: t.AccessToken,
+	}
+	return token, nil
+}
