44cf40af17942eea7d21a42a358ac698cbc4ff63
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2020-05-13 13:25:26 +0100

    Update proxy version in test
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

--- image_test.go
+++ image_test.go
@@ -6,7 +6,7 @@ func Test_GetInletsClientImage_DefaultOSSNoOverride(t *testing.T) {
 
 	c := InfraConfig{}
 	got := c.GetInletsClientImage()
-	want := "inlets/inlets:2.7.0"
+	want := "inlets/inlets:2.7.2"
 	if got != want {
 		t.Errorf("for OSS variant want %s, but got %s", want, got)
 		t.Fail()
